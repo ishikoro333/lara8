2f4ae50354a27f4329c6448a26f2b8822a6557c8
ishikoro333 <ishikoro33_33@yahoo.co.jp>
2022-01-22 19:08:52 +0900

    add livewire/index.blade

--- app/Http/Livewire/Students.php
+++ app/Http/Livewire/Students.php
@@ -3,11 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Student;
 
 class Students extends Component
 {
     public function render()
     {
-        return view('livewire.students');
+        $students = Student::orderBy('id', 'DESC') -> get();
+        return view('livewire.students.index', compact('students'));
     }
 }
